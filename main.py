7e8f89afabeaac882c95519701d7baafd605a8aa
Kate Johnson <johnson.kat.g@gmail.com>
2023-04-17 16:32:01 -0400

    example using Keys and click selector

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get(
-    "https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
-driver.implicitly_wait(8)
-my_element = driver.find_element('id', 'downloadButton')
-my_element.click()
 
+driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+driver.implicitly_wait(5)
+try:
+    no_button = driver.find_element('class name', 'at-cm-no-button')
+    no_button.click()
+except:
+    print('No element with this class name. Skipping ....')
 
-WebDriverWait(driver, 30).until(
-    EC.text_to_be_present_in_element(
-        (By.CLASS_NAME, 'progress-label'),  # Element filtration
-        'Complete!'  # The expected text
-    )
-)
+sum1 = driver.find_element('id', 'sum1')
+sum2 = driver.find_element('id', 'sum2')
+
+sum1.send_keys(Keys.NUMPAD1, Keys.NUMPAD5)
+sum2.send_keys(Keys.NUMPAD1, Keys.NUMPAD5)
+
+btn = driver.find_element_by_css_selector('button[onclick="return total()"]')
