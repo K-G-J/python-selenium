ddad3e9421f9212db4619e6ace936f54cdabda89
Kate Johnson <johnson.kat.g@gmail.com>
2023-04-17 16:44:44 -0400

    structure of bot project

---
+++ bot/booking/booking.py
@@ -0,0 +1,17 @@
+
+import booking.constants as const
+import os
+from selenium import webdriver
+
+
+class Booking(webdriver.Chrome):
+    def __init__(self, teardown=False):
+        super(Booking, self).__init__()
+        self.teardown = teardown
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
+
+    def land_first_page(self):
+        self.get(const.BASE_URL)
