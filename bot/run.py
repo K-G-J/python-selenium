0b4afee8f9991abdc68a02ad83c6be6214b1a24f
Kate Johnson <johnson.kat.g@gmail.com>
2023-04-17 17:14:52 -0400

    updates to deal searching

--- bot/run.py
+++ bot/run.py
@@ -5,3 +5,6 @@ with Booking() as bot:
     bot.land_first_page()
     bot.change_currency(currency='USD')
     bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date='2023-04-17', check_out_date='2023-04-24')
+    bot.select_adults(1)
+    bot.click_search()
